eaa2a2a8bdbda26f82d1c2319cd570f9bb9b689f
doronkina <liudmyla.doronkina0@gmail.com>
2020-04-13 13:38:28 +0300

    Updates

---
+++ precache-manifest.260c3d1cf6df601db4d430d4556fb87b.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a87a9e4c7cd91487d27982e6fd0c6b2",
+    "url": "/simple-calendar/index.html"
+  },
+  {
+    "revision": "12f3daff82c9a6ccd987",
+    "url": "/simple-calendar/static/css/main.7003f17f.chunk.css"
+  },
+  {
+    "revision": "576f6f58ea185a5a203a",
+    "url": "/simple-calendar/static/js/2.ceb3e58b.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/simple-calendar/static/js/2.ceb3e58b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12f3daff82c9a6ccd987",
+    "url": "/simple-calendar/static/js/main.3934a356.chunk.js"
+  },
+  {
+    "revision": "12b95d1a83a28d25a841",
+    "url": "/simple-calendar/static/js/runtime-main.a67069a8.js"
+  },
+  {
+    "revision": "20754144224d010a45724313c8faff78",
+    "url": "/simple-calendar/static/media/analytics.20754144.svg"
+  },
+  {
+    "revision": "fe8d3a55cc7f711d54b0b2783c836ef4",
+    "url": "/simple-calendar/static/media/arrowDown.fe8d3a55.svg"
+  },
+  {
+    "revision": "82ede4231665b3ee35e55a6321971a7a",
+    "url": "/simple-calendar/static/media/arrowLeft.82ede423.svg"
+  },
+  {
+    "revision": "2a4c1ccf959414b33319f64c76a83916",
+    "url": "/simple-calendar/static/media/arrowRight.2a4c1ccf.svg"
+  },
+  {
+    "revision": "74a1e2cc5006985b4bb16371ce23ba7a",
+    "url": "/simple-calendar/static/media/bell.74a1e2cc.svg"
+  },
+  {
+    "revision": "0529b8390962d996ef0c103a2224edc2",
+    "url": "/simple-calendar/static/media/broadcasts.0529b839.svg"
+  },
+  {
+    "revision": "f6672e1d68af6bf6a956a348a26c78f8",
+    "url": "/simple-calendar/static/media/cancel.f6672e1d.svg"
+  },
+  {
+    "revision": "f5630fbe6e2190aff419816b61d559b5",
+    "url": "/simple-calendar/static/media/edit.f5630fbe.svg"
+  },
+  {
+    "revision": "6fb6429600f32dcab64a2f588f8498f1",
+    "url": "/simple-calendar/static/media/logo.6fb64296.svg"
+  },
+  {
+    "revision": "a855ef1c3477088af54492919a38b263",
+    "url": "/simple-calendar/static/media/mailingLists.a855ef1c.svg"
+  },
+  {
+    "revision": "bfd085fc43f5ea0f5d2b2681a7262353",
+    "url": "/simple-calendar/static/media/marketing.bfd085fc.svg"
+  },
+  {
+    "revision": "a80ecac18ce8a9a34556e0b89365c5e7",
+    "url": "/simple-calendar/static/media/navIcon.a80ecac1.svg"
+  },
+  {
+    "revision": "57d854ccec046659d88adfed484cede7",
+    "url": "/simple-calendar/static/media/payments.57d854cc.svg"
+  },
+  {
+    "revision": "17a2e73fa7ee0bc6c2e7737062288fe1",
+    "url": "/simple-calendar/static/media/product.17a2e73f.svg"
+  },
+  {
+    "revision": "678baa598e3be668e9db1ca40c63b435",
+    "url": "/simple-calendar/static/media/searching.678baa59.svg"
+  },
+  {
+    "revision": "615adcffa28ab69d7a0ea416a15240c7",
+    "url": "/simple-calendar/static/media/settings.615adcff.svg"
+  },
+  {
+    "revision": "531c9d9d8c502728d943ea68b3d41c85",
+    "url": "/simple-calendar/static/media/siteBuilder.531c9d9d.svg"
+  },
+  {
+    "revision": "2531a105ce15f45e7677a4a8adb78a68",
+    "url": "/simple-calendar/static/media/trash.2531a105.svg"
+  },
+  {
+    "revision": "1efe1a53ce26894aba7b9382d0f65f2f",
+    "url": "/simple-calendar/static/media/users.1efe1a53.svg"
+  }
+]);
